386893cfceed0a8f9b0fd815ff8521953ba2f2d4
Tshepiso Rosa <132429932+Mafuyeka@users.noreply.github.com>
2024-09-03 21:05:10 +0200

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -5,29 +5,58 @@ class DBClient {
     const host = process.env.DB_HOST || 'localhost';
     const port = process.env.DB_PORT || 27017;
     const database = process.env.DB_DATABASE || 'files_manager';
-    const url = `mongodb+srv://gmarkd:UEO2AqP2DLHJEqab@manager.xrimd.mongodb.net/?retryWrites=true&w=majority&appName=manager`;
+    const url = process.env.MONGODB_URL || `mongodb://${host}:${port}/${database}`;
 
     this.client = new MongoClient(url, { useUnifiedTopology: true });
-    this.client.connect((err) => {
-      if (err) console.error(err);
-      else console.log('Connected to MongoDB');
-    });
+
+    this.client.connect()
+      .then(() => console.log('Connected to MongoDB'))
+      .catch(err => console.error('Failed to connect to MongoDB', err));
   }
 
-  isAlive() {
-    return this.client.isConnected();
+  async isAlive() {
+    try {
+      await this.client.db().command({ ping: 1 });
+      return true;
+    } catch (err) {
+      return false;
+    }
   }
 
   async nbUsers() {
-    return this.client.db().collection('users').countDocuments();
+    try {
+      return await this.client.db().collection('users').countDocuments();
+    } catch (err) {
+      console.error('Failed to count users', err);
+      throw err;
+    }
   }
 
   async nbFiles() {
-    return this.client.db().collection('files').countDocuments();
+    try {
+      return await this.client.db().collection('files').countDocuments();
+    } catch (err) {
+      console.error('Failed to count files', err);
+      throw err;
+    }
   }
 
   async getUser(query) {
-    return this.client.db().collection('users').findOne(query);
+    try {
+      return await this.client.db().collection('users').findOne(query);
+    } catch (err) {
+      console.error('Failed to get user', err);
+      throw err;
+    }
+  }
+
+  async close() {
+    try {
+      await this.client.close();
+      console.log('MongoDB connection closed');
+    } catch (err) {
+      console.error('Failed to close MongoDB connection', err);
+    }
   }
 }
 
