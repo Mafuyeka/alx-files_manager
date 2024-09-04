8d9344c3870d2e86303070bae757eb7be4775645
zhabwe254 <zhabwe@gmail.com>
2024-09-04 21:55:10 +0300

    a

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
-import { Router } from 'express';
-import AppController from '../controllers/AppController';
-import UsersController from '../controllers/UsersController';
-import AuthController from '../controllers/AuthController';
-import FilesController from '../controllers/FilesController';
+const express = require('express');
+const AppController = require('../controllers/AppController');
+const UsersController = require('../controllers/UsersController');
+const AuthController = require('../controllers/AuthController');
+const FilesController = require('../controllers/FilesController');
 
-const router = Router();
+const router = express();
+router.use(express.json());
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
