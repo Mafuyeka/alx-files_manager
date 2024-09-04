89a7a0b29a528e4556e07b80ade771dd36d25e4c
zhabwe254 <zhabwe@gmail.com>
2024-09-05 02:17:46 +0300

    a

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-import express from 'express';
-import routes from './routes/index';
+const express = require('express');
 
 const app = express();
+const indexRouter = require('./routes/index');
+
 const port = process.env.PORT || 5000;
 
-app.use(express.json());
-app.use('/', routes);
-
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+app.listen(port, console.log(`Server running on port ${port}`));
+app.use(express.json({ limit: '50mb' }));
+app.use('/', indexRouter);
+module.exports = app;
